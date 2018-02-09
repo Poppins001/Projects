4072b310b918f8cd6c530dc54fa7618b091fd391
Poppins <mgenn123@gmail.com>
2018-02-10 09:10:20 +1000

    Add open gym and keras-rl READONLY

---
+++ RL/READONLY-rl/agents/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from .dqn import DQNAgent, NAFAgent, ContinuousDQNAgent
+from .ddpg import DDPGAgent
+from .cem import CEMAgent
+from .sarsa import SarsaAgent, SARSAAgent
+from .a3c import ContinuousA3CAgent
